72cf4d0ad953cc406e1f0d2eff7bded48a86831e
BueukOwl <37366896+bueuk@users.noreply.github.com>
2018-05-11 22:11:48 +0700

    Add exception for nil:NillClass

--- soal_2.rb
+++ soal_2.rb
@@ -4,10 +4,14 @@ nama = gets
 @array = nama.split
 
 def text(f, m, l)
-  if @array.count > 2 
-    puts "#{f.capitalize} #{m.chars.first.capitalize + '.'} #{l.capitalize}"
-  else
-    puts "#{f.capitalize} #{m.capitalize}"
+  begin
+    if @array.count > 2 
+      puts "#{f.capitalize} #{m.chars.first.capitalize + '.'} #{l.capitalize}"
+    else
+      puts "#{f.capitalize} #{m.capitalize}"
+    end
+  rescue
+    puts 'Kamu harus memasukan nama depan, nama tengah(optional), dan nama belakang'
   end
   
 end
