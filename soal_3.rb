e2da577564a385c4aeb5530cdedabc3069055266
BueukOwl <37366896+bueuk@users.noreply.github.com>
2018-05-11 17:29:28 +0700

    Create soal_3.rb

---
+++ soal_3.rb
@@ -0,0 +1,18 @@
+puts 'User memasukan angka :'
+angka = gets.chomp
+
+begin
+  angka = Integer(angka)
+    
+  if (angka % 3 == 0) & (angka % 5 == 0)
+    puts 'FizzBuzz'
+  elsif angka % 3 == 0
+    puts 'Fizz'
+  elsif angka % 5 == 0
+    puts 'Buzz'
+  else
+    puts 'angka bukan kelipatan 3 atau 5'.upcase!
+  end
+rescue 
+  puts 'Harus angka!'
+end
