4048f3aa7d78db83a25bac2a5b3ce45ee8045f70
BueukOwl <37366896+bueuk@users.noreply.github.com>
2018-05-11 21:53:47 +0700

    Create soal_6.rb

---
+++ soal_6.rb
@@ -0,0 +1,50 @@
+puts 'Perhitungan: '
+hitung = gets.chomp.split
+
+
+class Calculator
+  def penjumlahan(a, b)
+    a + b
+  end
+
+  def pengurangan(a, b)
+    a - b
+  end
+
+  def perkalian(a, b)
+    a * b
+  end
+
+  def pembagian(a, b)
+    a / b
+  end
+end
+
+if hitung.count == 3
+  if hitung.grep(/\+/).any?
+    calc = Calculator.new
+    hasil_jumlah = calc.penjumlahan(hitung[0].to_i, hitung[2].to_i)
+    puts hasil_jumlah
+    
+  elsif hitung.grep(/\-/).any?
+    calc = Calculator.new
+    hasil_kurang = calc.pengurangan(hitung[0].to_i, hitung[2].to_i)
+    puts hasil_kurang
+
+  elsif hitung.grep(/\*/).any?
+    calc = Calculator.new
+    hasil_kali = calc.perkalian(hitung[0].to_i, hitung[2].to_i)
+    puts hasil_kali
+
+  elsif hitung.grep(/\//).any?
+    begin
+      calc = Calculator.new
+      hasil_bagi = calc.pembagian(hitung[0].to_i, hitung[2].to_i)
+      puts hasil_bagi
+    rescue
+      puts 'tidak bisa dibagi 0'
+    end
+  end
+else
+  puts 'Ada yang error! pisahkan perhitungan dengan spasi, misal 2 + 3 . Minimal dan maksimal perhitungan adalah dua nilai, lebih atau kurang dari dua nilai tidak dianggap!' 
+end
