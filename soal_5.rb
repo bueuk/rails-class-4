301b10c8a863b8d120113e25444cabc18ec0d33a
BueukOwl <37366896+bueuk@users.noreply.github.com>
2018-05-11 22:14:24 +0700

    Update soal_5.rb

--- soal_5.rb
+++ soal_5.rb
@@ -1,9 +1,8 @@
 puts 'Kalimat yang ingin dihitung hurufnya: '
 huruf = gets
 
+#Nyari di stackoverflow, link nya lupa
 huruf_to_hash_count_char = huruf.scan(/\w/).inject(Hash.new(0)){|h, c| h[c.downcase] += 1; h}
 huruf_to_hash_count_char.each do | k, v|
   puts "#{k} muncul #{v} kali"
 end
-
-#Ini soal paling sulit keknya wkwkw
