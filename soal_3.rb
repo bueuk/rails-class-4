e6806d8b1922e43e2980b7c8246e9257b7996070
Nandang <nandang@pasarhosting.co.id>
2018-05-14 18:36:29 +0700

    Penyesuian kode via method

--- soal_3.rb
+++ soal_3.rb
@@ -1,21 +1,25 @@
+def fizz_buzz(angka)
+  begin
+
+    angka = Integer(angka)
+      
+    if (angka % 3 == 0) & (angka % 5 == 0)
+      puts 'FizzBuzz'
+    elsif angka % 3 == 0
+      puts 'Fizz'
+    elsif angka % 5 == 0
+      puts 'Buzz'
+    else
+      puts 'angka bukan kelipatan 3 atau 5'.upcase!
+    end
+    
+  rescue 
+    puts 'Harus angka!'
+
+  end
+end
+
 puts 'User memasukan angka :'
 angka = gets.chomp
 
-begin
-
-  angka = Integer(angka)
-    
-  if (angka % 3 == 0) & (angka % 5 == 0)
-    puts 'FizzBuzz'
-  elsif angka % 3 == 0
-    puts 'Fizz'
-  elsif angka % 5 == 0
-    puts 'Buzz'
-  else
-    puts 'angka bukan kelipatan 3 atau 5'.upcase!
-  end
-  
-rescue 
-  puts 'Harus angka!'
-
-end
+fizz_buzz(angka)
