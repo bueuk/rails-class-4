3a4f8c89bdac1a85488ad815e2fbaa4ff3ac0c8d
Nandang <nandang@pasarhosting.co.id>
2018-05-16 13:10:09 +0700

    ubah nama method

--- soal_5.rb
+++ soal_5.rb
@@ -1,6 +1,6 @@
-def char(huruf)
-  count = huruf.scan(/\w/).inject(Hash.new(0)){|h, c| h[c.downcase] += 1; h}
-  count.each do | key, value |
+def count(huruf)
+  char = huruf.scan(/\w/).inject(Hash.new(0)){|h, c| h[c.downcase] += 1; h}
+  char.each do | key, value |
     puts "#{key} muncul #{value}"
   end
 end
@@ -8,4 +8,4 @@ end
 puts 'Kalimat yang ingin dihitung hurufnya: '
 huruf = gets
 
-char(huruf)
+count(huruf)
