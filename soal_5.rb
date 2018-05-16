20d453767ea1ac4eb68a23d08c71f6b757d5ebc3
Nandang <nandang@pasarhosting.co.id>
2018-05-16 13:07:23 +0700

    Perubahan code menggunakan method

--- soal_5.rb
+++ soal_5.rb
@@ -1,8 +1,11 @@
+def char(huruf)
+  count = huruf.scan(/\w/).inject(Hash.new(0)){|h, c| h[c.downcase] += 1; h}
+  count.each do | key, value |
+    puts "#{key} muncul #{value}"
+  end
+end
+
 puts 'Kalimat yang ingin dihitung hurufnya: '
 huruf = gets
 
-#Nyari di stackoverflow, link nya lupa
-huruf_to_hash_count_char = huruf.scan(/\w/).inject(Hash.new(0)){|h, c| h[c.downcase] += 1; h}
-huruf_to_hash_count_char.each do | k, v|
-  puts "#{k} muncul #{v} kali"
-end
+char(huruf)
